0c9b9858b5d5a26454e4ad2934715f3294eeb827
Colin <colinclissonhg@gmail.com>
2020-04-15 19:14:23 -0400

    f

--- app/src/views/Episodes.vue
+++ app/src/views/Episodes.vue
@@ -1,9 +1,9 @@
 <template>
     <b-container class="content px-0 pt-4 shadow" v-if="episodes">
         <div class="content-box pt-3">
-            <video-player :class="{'landscape-video' : landscapeOriented()}" :id="currentEpisode.id" :name="landscapeOriented() ? null : currentEpisode.name"></video-player>
+            <video-player :id="currentEpisode.id" :name="currentEpisode.name"></video-player>
 
-            <div class="pr-2 pr-lg-0 pt-3 float-right">
+            <div class="pr-2 pr-lg-0 py-3 float-right">
                 <span class="text-white" style="font-size: 15pt">Choisir épisode : </span>
                 <b-select class="episode-chooser" v-model="$store.state.episode" @input="$store.commit('setEpisode', currentEpisode.name)">
                     <option v-for="episode in episodes" :key="episode.name">
@@ -25,17 +25,6 @@
             episodes() {
                 return this.$store.getters.episodes;
             }
-        },
-        created() {
-            let vm = this;
-            window.addEventListener("orientationchange", function() {
-                vm.$forceUpdate();
-            });
-        },
-        methods: {
-            landscapeOriented() {
-                return this.isMobileDevice() && screen.orientation.type === 'landscape-primary';
-            }
         }
     }
 </script>
@@ -51,15 +40,6 @@
 
             @media screen and (max-width: 1000px) {
                 width: 100%;
-            }
-
-            .landscape-video {
-                z-index: 5;
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
             }
         }
     }
